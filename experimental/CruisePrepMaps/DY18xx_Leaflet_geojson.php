7fa4be679d75c624f0136d11cb7b98d41d906aa6
shaunwbell <shaun.bell@noaa.gov>
2021-03-11 13:09:29 -0800

    Add experimental pages (#13)

---
+++ experimental/CruisePrepMaps/DY18xx_Leaflet_geojson.php
@@ -0,0 +1,167 @@
+<html>
+<head>
+<link rel="stylesheet" href="http://cdn.leafletjs.com/leaflet/v1.0.1/leaflet.css" />
+
+<style>#map { width: 800px; height: 500px; }
+.info { padding: 6px 8px; font: 14px/16px Arial, Helvetica, sans-serif; background: white; background: rgba(255,255,255,0.8); box-shadow: 0 0 15px rgba(0,0,0,0.2); border-radius: 5px; } .info h4 { margin: 0 0 5px; color: #777; }
+.legend { text-align: left; line-height: 18px; color: #555; } .legend i { width: 18px; height: 18px; float: left; margin-right: 8px; opacity: 0.7; }
+</style>
+
+<script type="text/javascript"  src="../../scripts/leaflet/leaflet.js"></script>
+<script type="text/javascript"  src="../../scripts/leaflet/leaflet.ajax.min.js"></script>
+<script type="text/javascript"  src="../../scripts/jquery/jquery.min.js"></script>
+<script type="text/javascript"  src="http://maps.stamen.com/js/tile.stamen.js?v1.3.0"></script>
+</head>
+<body>
+
+<div id="mapid" style="width: 100%; height: 100%"></div>
+
+    <script>
+
+        // get color depending on population density value
+        function getColor(d) {
+            return d == 'Moorings' ? '#009933' :
+                    d == 'CTD/BON'  ? '#D54339' :
+                    d == 'CTD'  ? '#660066' :
+                    d == 'Calvets'   ? '#808080' :
+                    d == 'Glider'   ? '#FFFF00' :
+                                '#FFEDA0';
+        }
+
+        var Esri_OceanBasemap = new L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/Ocean_Basemap/MapServer/tile/{z}/{y}/{x}', {
+            attribution: 'Tiles &copy; Esri &mdash; Sources: GEBCO, NOAA, CHS, OSU, UNH, CSUMB, National Geographic, DeLorme, NAVTEQ, and Esri',
+            maxZoom: 13
+        });
+        //http://maps.ngdc.noaa.gov/arcgis/rest/services/web_mercator/gebco08_contours/MapServer/tile/{z}/{y}/{x}
+        // replace "toner" here with "terrain" or "watercolor"
+        //var layer = new L.StamenTileLayer("toner");
+        var mymap = new L.Map("mapid", {
+            center: new L.LatLng(55.5, -178.3),
+            zoom: 4
+        });
+        mymap.addLayer(Esri_OceanBasemap);    
+
+        var geojsonMarkerOptions = {
+            radius: 4,
+            fillColor: "#D54339",
+            color: "#ffffff",
+            weight: .2,
+            opacity: 1,
+            fillOpacity: 0.8
+        };
+
+        var geojsonLayer = new L.GeoJSON.AJAX("<?php echo 'DY18xx.geojson'?>", {
+            pointToLayer: function (feature, latlng) {
+                return L.circleMarker(latlng, geojsonMarkerOptions);
+            },
+            onEachFeature: function (feature, layer) {
+            layer.bindPopup(feature.properties.FIELD1 + ' ' + feature.properties.FIELD2);
+            }
+        }); 
+
+        geojsonLayer.addTo(mymap);
+
+        var geojsonMarkerOptions_ctdonly = {
+            radius: 4,
+            fillColor: "#660066",
+            color: "#ffffff",
+            weight: .2,
+            opacity: 1,
+            fillOpacity: 0.8
+        };
+
+        var geojsonLayer = new L.GeoJSON.AJAX("<?php echo 'DY18xx_ctdonly.geojson'?>", {
+            pointToLayer: function (feature, latlng) {
+                return L.circleMarker(latlng, geojsonMarkerOptions_ctdonly);
+            },
+            onEachFeature: function (feature, layer) {
+            layer.bindPopup(feature.properties.FIELD1 + ' ' + feature.properties.FIELD2);
+            }
+        });
+
+
+        geojsonLayer.addTo(mymap);
+
+        var geojsonMarkerOptions_mooring = {
+            radius: 4,
+            fillColor: "#009933",
+            color: "#ffffff",
+            weight: .2,
+            opacity: 1,
+            fillOpacity: 0.8
+        };
+
+        var geojsonLayer = new L.GeoJSON.AJAX("<?php echo 'DY18xx_moorings.geojson'?>", {
+            pointToLayer: function (feature, latlng) {
+                return L.circleMarker(latlng, geojsonMarkerOptions_mooring);
+            },
+            onEachFeature: function (feature, layer) {
+            layer.bindPopup(feature.properties.FIELD1 + ' ' + feature.properties.FIELD2);
+            }
+        });
+
+        geojsonLayer.addTo(mymap);
+
+
+
+        var geojsonMarkerOptions_calvets = {
+            radius: 4,
+            fillColor: "#808080",
+            color: "#ffffff",
+            weight: .2,
+            opacity: 1,
+            fillOpacity: 0.8
+        };
+
+        var geojsonLayer = new L.GeoJSON.AJAX("<?php echo 'DY18xx_calvets.geojson'?>", {
+            pointToLayer: function (feature, latlng) {
+                return L.circleMarker(latlng, geojsonMarkerOptions_calvets);
+            },
+            onEachFeature: function (feature, layer) {
+            layer.bindPopup(feature.properties.FIELD1 + ' ' + feature.properties.FIELD2);
+            }
+        });
+
+        var geojsonMarkerOptions_Glider = {
+            radius: 4,
+            fillColor: "#FFFF00",
+            color: "#ffffff",
+            weight: .2,
+            opacity: 1,
+            fillOpacity: 0.8
+        };
+
+        var geojsonLayer = new L.GeoJSON.AJAX("<?php echo 'DY18xx_gliders.geojson'?>", {
+            pointToLayer: function (feature, latlng) {
+                return L.circleMarker(latlng, geojsonMarkerOptions_Glider);
+            },
+            onEachFeature: function (feature, layer) {
+            layer.bindPopup(feature.properties.FIELD1 + ' ' + feature.properties.FIELD2);
+            }
+        });
+
+
+        geojsonLayer.addTo(mymap);        
+
+        var legend = L.control({position: 'bottomleft'});
+
+        legend.onAdd = function (map) {
+
+            var div = L.DomUtil.create('div', 'info legend'),
+                categories = ['Moorings','CTD/BON','CTD','Calvets','Glider'];
+
+            // loop through our density intervals and generate a label with a colored square for each interval
+            for (var i = 0; i < categories.length; i++) {
+                div.innerHTML +=
+                    '<i style="background:' + getColor(categories[i]) + '"></i> ' +
+                    (categories[i] ? categories[i ] + '<br>' : '+');
+            }
+
+            return div;
+        };
+
+        legend.addTo(mymap);
+
+    </script>
+</body>
+</html>
---
+++ experimental/index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>EcoFOCI Experimental Datastreams</title>
+<!-- Bootstrap theme -->
+<link rel="stylesheet" type="text/css" id="bootstrap.css" href="../bootstrap/dist/css/bootstrap.css.spacelab.V3.css" />
+<link rel="stylesheet" type="text/css" id="custom.css" href="../scripts/bootstrap_datatables/bootstrap_datatables.css" />
+<link rel="stylesheet" type="text/css" id="custom.css" href="../scripts/css/custom.css" />
+
+<!-- when using the dataTables javascript plugin, jquery and dataTables must be at top of page -->
+<!-- dataTables seems to only work well with a single table on a page-->
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="../scripts/jquery_1.11.0/jquery.min.js"></script>
+
+</head>
+
+<body>
+
+
+<!-- local php includes
+================================================== -->
+<?php 
+include('../ecofoci_images/header.php'); 
+include('php_routines/carousel.php');
+include('php_routines/nav_header.php');
+?>
+
+<!-- Bootstrap - Static navbar
+================================================== -->
+<div class="container">
+
+
+<!-- Static navbar -->
+<?php build_navbar("Home") ?>
+
+<!-- End Navigation
+================================================== -->
+
+
+<div class="col-md-12 center-block" style="padding:20px;">
+
+<!-- Carousel
+================================================== -->
+<div class="col-md-8 center-block" style="padding:20px;">
+
+<?php carousel_images() ?>
+
+</div>
+<!-- end carousel
+================================================== -->
+
+
+    <p class="text-muted"><?php include('../ecofoci_images/footer.php'); ?></p>
+</div>
+
+<!-- end footer
+================================================== -->
+
+</div>
+
+<noscript><div >This Website requires your browser to be JavaScript enabled.</div></noscript>
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="../bootstrap/dist/js/bootstrap.min.js"></script>
+
+<script src="../scripts/jquery.backstretch.js"></script>
+<script>
+    $.backstretch(["../ecofoci_images/saildrone/SailDrone.png"]);
+</script>
+
+</body>
+</html>
